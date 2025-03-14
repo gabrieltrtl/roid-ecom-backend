138f059842b5e82b427c7ddb6122c654317f764d
Gabriel Lira <gabriel.tertulino@proton.me>
2025-03-14 15:28:29 -0300

    v2.8

---
+++ src/models/Company.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const CompanySchema = new mongoose.Schema({
+name: { type: String, required: true },
+domain: {type: String, required: true },
+email: { type: String, required: true, unique: true },
+password: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Company', CompanySchema);
