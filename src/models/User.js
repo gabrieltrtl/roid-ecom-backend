6ebea7d5d5604ceb68743707db4513d463294ec8
Gabriel Lira <gabriel.tertulino@proton.me>
2025-03-19 16:33:49 -0300

    v5.2

--- src/models/User.js
+++ src/models/User.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  email: { type: String, required: true },
   password: { type: String, required: true },
   role: {  
     type: String,
@@ -24,5 +24,8 @@ UserSchema.pre('save', async function (next) {
 UserSchema.methods.comparePassword = async function (password) {
   return await bcrypt.compare(password, this.password);
 };
+
+// 🔹 Adicionamos um índice para impedir e-mails duplicados dentro da mesma empresa
+UserSchema.index({ email: 1, companyId: 1 }, { unique: true });
 
 module.exports = mongoose.model('User', UserSchema);
