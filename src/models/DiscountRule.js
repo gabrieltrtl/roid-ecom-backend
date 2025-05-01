80ed1b90ef8af7ae3d16645a5998fcd684e4c8e2
Gabriel Lira <gabriel.tertulino@proton.me>
2025-05-01 10:27:16 -0300

    v11.1 - DiscountRule.js Changes

--- src/models/DiscountRule.js
+++ src/models/DiscountRule.js
@@ -1,5 +1,31 @@
 const mongoose = require('mongoose');
 
+const conditionsSchema = new mongoose.Schema({
+  field: { type: String, required: true }, // Name, Price, Stock, Sku, etc...
+  operator: {
+    type: String,
+    enum: ['equals', 'contains', 'gt', 'lt', 'gte', 'lte'],
+    required: true
+  },
+  value: { type: mongoose.Schema.Types.Mixed, required: true }
+
+}, { _id: false });
+
+const ruleSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    enum: ['percentage', 'fixed', 'override'],
+    required: true
+  },
+  value: { type: Number, required: true },
+  logic: {
+    type: String,
+    enum: ['and', 'or', 'none'],
+    default: 'none'
+  },
+  conditions: [conditionsSchema]
+}, { _id: false })
+
 const discountRuleSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -8,17 +34,7 @@ const discountRuleSchema = new mongoose.Schema({
   description: {
     type: String
   },
-  type: {
-    type: String,
-    enum: ['percentage', 'fixed', 'override'],
-    required: true,
-  },
-  value: {
-    type: Number,
-    required: true,
-  },
-  includedProducts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }], // ✅ Produtos incluídos
-  excludedProducts: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Product' }],
+  rules: [ruleSchema],
   company: { type: mongoose.Schema.Types.ObjectId, ref: 'Company', required: true }, // ✅ Produtos excluídos
 }, { timestamps: true });
 
