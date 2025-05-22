bf35fd3207f40538ed6de7a2a9111ba74035fb88
Gabriel Lira <gabriel.tertulino@proton.me>
2025-05-22 08:42:49 -0300

    13.5 - Changes in company.js Model

--- src/models/Company.js
+++ src/models/Company.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
 const CompanySchema = new mongoose.Schema({
-name: { type: String, required: true }
+name: { type: String, required: true },
+
+// 📦 Integração com Z-API
+  zapiInstanceId: { type: String, default: null }, // ID da instância logada via QR Code
+  zapiToken: { type: String, default: null } 
 },  { timestamps: true } );
 
 module.exports = mongoose.model('Company', CompanySchema);
